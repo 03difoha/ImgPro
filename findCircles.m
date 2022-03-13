b2f96d817319f2f0dde0ee549342a9b6abae91b5
03difoha <h@difol.co>
2022-03-13 21:33:34 +0000

    threshold for warped sircles

--- findCircles.m
+++ findCircles.m
@@ -1,6 +1,7 @@
 function [centers] = findCircles(img)
     disp("running findCircles")
-    [centers, radii] = imfindcircles(img,[10, 50], 'Sensitivity',0.9, 'ObjectPolarity','dark');
+    [centers, radii] = imfindcircles(img,[10, 50], 'Sensitivity',0.8, 'ObjectPolarity','dark');
+    imshow(img)
     viscircles(centers, radii,'Color','b');
 end
 
