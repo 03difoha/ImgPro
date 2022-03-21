94b0870495882228dc23b045dc85fae5db625355
03difoha <h@difol.co>
2022-03-21 23:03:33 +0000

    begun registration

--- findCircles.m
+++ findCircles.m
@@ -1,5 +1,4 @@
 function [centers, radii] = findCircles(img)
-    disp("running findCircles")
 %      [centers, radii] = imfindcircles(img,[10, 50], 'Sensitivity',0.8, 'ObjectPolarity','dark');
 %      imshow(img)
 %      viscircles(centers, radii,'Color','b');
