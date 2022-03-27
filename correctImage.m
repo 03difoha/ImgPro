e237a8f6b398c35e0a5cc90f5c2600ff036ad3d9
03difoha <h@difol.co>
2022-03-27 13:58:04 +0100

    cropped image and split up into 3D cells

--- correctImage.m
+++ correctImage.m
@@ -1,10 +1,23 @@
 function [Jregistered] = correctImage(image, fixedPoints, movingPoints, I)
-     if size(fixedPoints) == size(movingPoints)
-         tform = fitgeotrans(movingPoints,fixedPoints,'projective');
-         Jregistered = imwarp(image,tform,'OutputView',imref2d(size(I)));
-     else
+%  fixedPoints = [26.5 26.5; 26.5 445.5; 445.5  445.5; 445.5 26.5 ];
+    warped = false;
+    disp(movingPoints)
+    for i = 1:size(movingPoints,1)
+        xWarped = (movingPoints(i,1) - 26.5 >= 3) | (movingPoints(i,1) - 445.5 >= 3) ;
+        yWarped = (movingPoints(i,2) - 26.5 >= 3) | (movingPoints(i,2) - 445.5 >= 3) ;
+        if (logical(xWarped) & logical(yWarped))
+            warped = true;
+        end
+    end
+
+    if size(fixedPoints) == size(movingPoints) & warped
+        tform = fitgeotrans(movingPoints,fixedPoints,'projective');
+        Jregistered = imwarp(image,tform,'OutputView',imref2d(size(I)));
+    else
         Jregistered = image;
-     end
+    end
+        disp(warped)
+        disp('----------------')
 end
 
 
